11b825465232dec09feb4a2e23b22ffe51cbff43
William Hleucka <william.hleucka@gmail.com>
2022-07-27 16:29:47 -0600

    Update telescope conf

--- nvim/.config/nvim/lua/whleucka/telescope.lua
+++ nvim/.config/nvim/lua/whleucka/telescope.lua
@@ -1,24 +1,26 @@
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 require('telescope').setup {
-    prompt_prefix = " ",
-    selection_caret = "❯ ",
-    path_display = { "truncate" },
-    selection_strategy = "reset",
-    sorting_strategy = "ascending",
-    layout_strategy = "horizontal",
-    layout_config = {
-      horizontal = {
-        prompt_position = "top",
-        preview_width = 0.55,
-        results_width = 0.8,
-      },
-      vertical = {
-        mirror = false,
-      },
-      width = 0.87,
-      height = 0.80,
-      preview_cutoff = 120,
+    defaults = {
+        prompt_prefix = " ",
+        selection_caret = "❯ ",
+        path_display = { "truncate" },
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        },
     },
     extensions = {
         fzf = {
