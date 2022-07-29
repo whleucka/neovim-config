9d1a11bb59e647902b4d471bd816907fa7a2bba8
William Hleucka <william.hleucka@gmail.com>
2022-07-29 14:13:07 -0600

    Cache plugins

--- nvim/.config/nvim/lua/whleucka/autocmds.lua
+++ nvim/.config/nvim/lua/whleucka/autocmds.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
     pattern = '*',
     callback = function()
         if vim.bo.filetype == 'NvimTree' then
-            require 'bufferline.state'.set_offset(31, 'FileTree')
+            require'bufferline.state'.set_offset(31, 'FileTree')
         end
     end
 })
@@ -12,12 +12,12 @@ vim.api.nvim_create_autocmd('BufWinLeave', {
     pattern = '*',
     callback = function()
         if vim.fn.expand('<afile>'):match('NvimTree') then
-            require 'bufferline.state'.set_offset(0)
+            require'bufferline.state'.set_offset(0)
         end
     end
 })
 
-local save_pos = vim.api.nvim_create_augroup("SaveIt", { clear = true })
+local save_pos = vim.api.nvim_create_augroup("SaveIt", {clear = true})
 vim.api.nvim_create_autocmd("BufRead", {
     desc = "Restore last cursor position",
     group = save_pos,
@@ -25,7 +25,15 @@ vim.api.nvim_create_autocmd("BufRead", {
     command = [[call setpos(".", getpos("'\""))]]
 })
 
-local leave = vim.api.nvim_create_augroup("LeaveIt", { clear = true })
+local packer_sync = vim.api.nvim_create_augroup("PackerSyncIt", {clear = true})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    desc = "Auto save plugins",
+    group = packer_sync,
+    pattern = 'plugins.lua',
+    command = "source <afile> | PackerSync"
+})
+
+local leave = vim.api.nvim_create_augroup("LeaveIt", {clear = true})
 vim.api.nvim_create_autocmd("VimLeave", {
     desc = "Set cursor position on exit",
     group = leave,
@@ -33,7 +41,7 @@ vim.api.nvim_create_autocmd("VimLeave", {
     command = "wshada!"
 })
 
-local yank = vim.api.nvim_create_augroup("YankIt", { clear = true })
+local yank = vim.api.nvim_create_augroup("YankIt", {clear = true})
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Hightlight text on yank",
     group = yank,
@@ -42,69 +50,69 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Integrate lsp progress with nvim-notify
-vim.api.nvim_create_autocmd({ 'UIEnter' }, {
-    once = true,
-    callback = function()
-        local Spinner = require('whleucka.spinner')
-        local spinners = {}
-
-        local function format_msg(msg, percentage)
-            msg = msg or ''
-            if not percentage then
-                return msg
-            end
-            return string.format('%2d%%\t%s', percentage, msg)
-        end
-
-        vim.api.nvim_create_autocmd({ 'User' }, {
-            pattern = { 'LspProgressUpdate' },
-            group = vim.api.nvim_create_augroup('LSPNotify', { clear = true }),
-            desc = 'LSP progress notifications',
-            callback = function()
-                for _, c in ipairs(vim.lsp.get_active_clients()) do
-                    for token, ctx in pairs(c.messages.progress) do
-                        if not spinners[c.id] then
-                            spinners[c.id] = {}
-                        end
-                        local s = spinners[c.id][token]
-                        if not ctx.done then
-                            if not s then
-                                spinners[c.id][token] = Spinner(
-                                    format_msg(ctx.message, ctx.percentage), vim.log.levels.INFO, {
-                                    title = ctx.title and string.format('%s: %s', c.name, ctx.title) or c.name
-                                })
-                            else
-                                s:update(format_msg(ctx.message, ctx.percentage))
-                            end
-                        else
-                            c.messages.progress[token] = nil
-                            if s then
-                                s:done(ctx.message or 'Complete', nil, {
-                                    icon = '',
-                                })
-                                spinners[c.id][token] = nil
-                            end
-                        end
-                    end
-                end
-            end,
-        })
-    end,
-})
+--vim.api.nvim_create_autocmd({'UIEnter'}, {
+--    once = true,
+--    callback = function()
+--        local Spinner = require('whleucka.spinner')
+--        local spinners = {}
+--
+--        local function format_msg(msg, percentage)
+--            msg = msg or ''
+--            if not percentage then
+--                return msg
+--            end
+--            return string.format('%2d%%\t%s', percentage, msg)
+--        end
+--
+--        vim.api.nvim_create_autocmd({'User'}, {
+--            pattern = {'LspProgressUpdate'},
+--            group = vim.api.nvim_create_augroup('LSPNotify', {clear = true}),
+--            desc = 'LSP progress notifications',
+--            callback = function()
+--                for _, c in ipairs(vim.lsp.get_active_clients()) do
+--                    for token, ctx in pairs(c.messages.progress) do
+--                        if not spinners[c.id] then
+--                            spinners[c.id] = {}
+--                        end
+--                        local s = spinners[c.id][token]
+--                        if not ctx.done then
+--                            if not s then
+--                                spinners[c.id][token] = Spinner(
+--                                format_msg(ctx.message, ctx.percentage), vim.log.levels.INFO, {
+--                                    title = ctx.title and string.format('%s: %s', c.name, ctx.title) or c.name
+--                                })
+--                            else
+--                                s:update(format_msg(ctx.message, ctx.percentage))
+--                            end
+--                        else
+--                            c.messages.progress[token] = nil
+--                            if s then
+--                                s:done(ctx.message or 'Complete', nil, {
+--                                    icon = '',
+--                                })
+--                                spinners[c.id][token] = nil
+--                            end
+--                        end
+--                    end
+--                end
+--            end,
+--        })
+--    end,
+--})
 
 -- Will work in v0.8.0
 --local lsp = vim.api.nvim_create_augroup("LSP", { clear = true })
 --vim.api.nvim_create_autocmd("LspAttach", {
---    group = lsp,
---    callback = function()
---        print("With great power comes great responsibility")
---    end,
---})
+    --    group = lsp,
+    --    callback = function()
+        --        print("With great power comes great responsibility")
+        --    end,
+        --})
 
--- Folds
---local folds = vim.api.nvim_create_augroup("FoldIt", {clear = true})
---vim.api.nvim_create_autocmd("BufReadPost,FileReadPost", {
---    group = folds,
---    pattern = '*',
---    command = "normal zR"
---})
+        -- Folds
+        --local folds = vim.api.nvim_create_augroup("FoldIt", {clear = true})
+        --vim.api.nvim_create_autocmd("BufReadPost,FileReadPost", {
+            --    group = folds,
+            --    pattern = '*',
+            --    command = "normal zR"
+            --})
--- nvim/.config/nvim/lua/whleucka/init.lua
+++ nvim/.config/nvim/lua/whleucka/init.lua
@@ -1,3 +1,4 @@
+require("impatient").enable_profile()
 require("whleucka.plugins")
 require("whleucka.options")
 require("whleucka.mappings")
