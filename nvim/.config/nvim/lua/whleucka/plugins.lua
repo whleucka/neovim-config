943f0011a7bfa5cb70ce80371d6cf5a3a7a92cd5
William Hleucka <william.hleucka@gmail.com>
2022-08-05 09:08:45 -0600

    Enable impatient

--- nvim/.config/nvim/lua/whleucka/plugins.lua
+++ nvim/.config/nvim/lua/whleucka/plugins.lua
@@ -32,7 +32,7 @@ return packer.startup({function(use)
     use "wbthomason/packer.nvim"
 
     -- Cache plugins
-    --use "lewis6991/impatient.nvim"
+    use "lewis6991/impatient.nvim"
 
     -- Theme
     use "folke/tokyonight.nvim"
