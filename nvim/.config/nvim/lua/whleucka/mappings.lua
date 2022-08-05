cf7c8cbc6564b7088ed7d0d3cb4dd8b4847c4ed4
William Hleucka <william.hleucka@gmail.com>
2022-08-05 11:07:49 -0600

    Update with missing mappings

--- nvim/.config/nvim/lua/whleucka/mappings.lua
+++ nvim/.config/nvim/lua/whleucka/mappings.lua
@@ -1,7 +1,10 @@
 local nnoremap = require("whleucka.remap").nnoremap
 local inoremap = require("whleucka.remap").inoremap
+local xnoremap = require("whleucka.remap").xnoremap
+local vnoremap = require("whleucka.remap").vnoremap
 
 -- Quick save / exit
+nnoremap("<leader>le", ":Lex 50<CR>")
 nnoremap("<leader>w", "<cmd> update<CR>")
 nnoremap("<leader>q", "<cmd> q<CR>")
 nnoremap("<leader>c", "<cmd> BufferDelete<CR>")
@@ -39,11 +42,25 @@ nnoremap("<leader>fw", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
 nnoremap("<leader>I", "<cmd> Mason<CR>")
 
 -- Exit insert
-inoremap("jk", "<ESC>");
-inoremap("kj", "<ESC>");
+inoremap("jk", "<ESC>")
+inoremap("kj", "<ESC>")
 
 -- File navigation
 nnoremap("<leader>e", "<cmd> NvimTreeToggle<CR>")
+
+-- Avoid Ex mode
+nnoremap("Q", "<nop>")
+
+-- Break undo sequence
+inoremap(".", ".<C-g>u")
+inoremap("!", "!<C-g>u")
+inoremap("?", "?<C-g>u")
+inoremap(",", ",<C-g>u")
+inoremap("$", "$<C-g>u")
+inoremap("(", "(<C-g>u")
+inoremap(")", ")<C-g>u")
+inoremap("[", "[<C-g>u")
+inoremap("]", "]<C-g>u")
 
 -- Split navigation
 nnoremap("<leader>h", "<C-w><C-h>")
@@ -52,6 +69,46 @@ nnoremap("<leader>k", "<C-w><C-k>")
 nnoremap("<leader>l", "<C-w><C-l>")
 nnoremap("<leader><leader>", "<C-w><C-p>")
 
+-- Resize splits
+nnoremap("<A-Up>", ":resize +2<CR>")
+nnoremap("<A-Down>", ":resize -2<CR>")
+nnoremap("<A-Left>", ":vertical resize +2<CR>")
+nnoremap("<A-Right>", ":vertical resize -2<CR>")
+
+-- Line movements
+nnoremap("H", "^")
+nnoremap("L", "$")
+
+-- Stay in indent mode
+vnoremap("<", "<gv")
+vnoremap(">", ">gv")
+-- Reselect visual when indenting
+xnoremap("<", "<gv")
+xnoremap(">", ">gv")
+
+-- ThePrimeagen (Yayayayayayaya)
+-- Move visual lines up or down with J,K
+vnoremap("J", ":m '>+1<CR>gv=gv")
+vnoremap("K", ":m '<-2<CR>gv=gv")
+-- Keeping it centered
+nnoremap("Y", "yg$")
+nnoremap("n", "nzzzv")
+nnoremap("N", "Nzzzv")
+nnoremap("J", "mzJ`z")
+-- Yank/Paste/Delete
+nnoremap("<leader>y", '"+y')
+vnoremap("<leader>y", '"+y')
+nnoremap("<leader>y", 'gg"+yG')
+xnoremap("<leader>p", '"_dP')
+nnoremap("<leader>d", '"_d')
+vnoremap("<leader>d", '"_d')
+
+-- Clipboard
+
 -- Tabs
-nnoremap("H", "<cmd> BufferPrev<CR>")
-nnoremap("L", "<cmd> BufferNext<CR>")
+nnoremap("<M-,>", "<cmd> BufferPrev<CR>")
+nnoremap("<M-.>", "<cmd> BufferNext<CR>")
+
+-- Quickfix lists
+nnoremap("[q", ":cprevious<CR>")
+nnoremap("]q", ":cnext<CR>")
--- nvim/.config/nvim/lua/whleucka/options.lua
+++ nvim/.config/nvim/lua/whleucka/options.lua
@@ -26,6 +26,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.scrolloff = 5
 
 -- Splits
 vim.opt.splitbelow = true
