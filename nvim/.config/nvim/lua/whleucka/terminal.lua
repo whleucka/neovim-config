02332d1e65eec4931e66792dd081c5976e4f711b
William Hleucka <william.hleucka@gmail.com>
2022-08-18 10:37:23 -0600

    Add dbu hook

--- nvim/.config/nvim/lua/whleucka/terminal.lua
+++ nvim/.config/nvim/lua/whleucka/terminal.lua
@@ -5,6 +5,7 @@ local Terminal  = require('toggleterm.terminal').Terminal
 local bpytop = Terminal:new({ cmd = "bpytop", hidden = true, direction = "float" })
 local htop = Terminal:new({ cmd = "htop", hidden = true, direction = "float" })
 local matrix = Terminal:new({ cmd = "cmatrix -ba -u 2", hidden = true, direction = "float" })
+local dbu = Terminal:new({ cmd = "/usr/bin/php ./db-upgrade.php", hidden = true, direction = "vertical", size = 60 })
 
 function M.bpytop_toggle()
     bpytop:toggle()
@@ -16,6 +17,10 @@ end
 
 function M.matrix_toggle()
     matrix:toggle()
+end
+
+function M.dbu_toggle()
+    dbu:toggle()
 end
 
 return M
