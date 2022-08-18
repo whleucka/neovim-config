9c190634f4f6c896cbf28c6be91f63cf6f6d264c
William Hleucka <william.hleucka@gmail.com>
2022-08-18 10:59:19 -0600

    Update dbu command

--- nvim/.config/nvim/lua/whleucka/terminal.lua
+++ nvim/.config/nvim/lua/whleucka/terminal.lua
@@ -5,7 +5,11 @@ local Terminal  = require('toggleterm.terminal').Terminal
 local bpytop = Terminal:new({ cmd = "bpytop", hidden = true, direction = "float" })
 local htop = Terminal:new({ cmd = "htop", hidden = true, direction = "float" })
 local matrix = Terminal:new({ cmd = "cmatrix -ba -u 2", hidden = true, direction = "float" })
-local dbu = Terminal:new({ cmd = "/usr/bin/php -r '$cms_public_page = true; include_once \"./db-upgrade.php\"; sleep(5);'", hidden = true, direction = "float" })
+local dbu = Terminal:new({
+    cmd = "/usr/bin/php -r '$cms_public_page = true; ob_start(); include_once \"./db-upgrade.php\"; $output = ob_get_contents(); echo str_replace(\"<br/>\", \"\", $output); sleep(5);'",
+    hidden = true,
+    direction = "float"
+})
 
 function M.bpytop_toggle()
     bpytop:toggle()
