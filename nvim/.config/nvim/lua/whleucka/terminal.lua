e02f203f93ee37a5ec87ad6b62d48d37f2e88fac
William Hleucka <william.hleucka@gmail.com>
2022-08-26 09:19:05 -0600

    Update neovim config

--- nvim/.config/nvim/lua/whleucka/terminal.lua
+++ nvim/.config/nvim/lua/whleucka/terminal.lua
@@ -5,11 +5,7 @@ local Terminal  = require('toggleterm.terminal').Terminal
 local bpytop = Terminal:new({ cmd = "bpytop", hidden = true, direction = "float" })
 local htop = Terminal:new({ cmd = "htop", hidden = true, direction = "float" })
 local matrix = Terminal:new({ cmd = "cmatrix -ba -u 2", hidden = true, direction = "float" })
-local dbu = Terminal:new({
-    cmd = "/usr/bin/php -r '$cms_public_page = true; ob_start(); include_once \"./db-upgrade.php\"; $output = ob_get_contents(); ob_end_clean(); echo str_replace(\"<br/>\", \"\", $output); sleep(5);'",
-    hidden = true,
-    direction = "float"
-})
+local ncdu = Terminal:new({ cmd = "ncdu .", hidden = true, direction = "float" })
 
 function M.bpytop_toggle()
     bpytop:toggle()
@@ -23,8 +19,8 @@ function M.matrix_toggle()
     matrix:toggle()
 end
 
-function M.dbu_toggle()
-    dbu:toggle()
+function M.ncdu_toggle()
+    ncdu:toggle()
 end
 
 return M
