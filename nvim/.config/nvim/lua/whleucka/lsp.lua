4aa088fd17d325f78e4c17c9298e352d05d807ff
William Hleucka <william.hleucka@gmail.com>
2022-10-21 21:43:23 -0600

    Astro type lsp

--- nvim/.config/nvim/lua/whleucka/lsp.lua
+++ nvim/.config/nvim/lua/whleucka/lsp.lua
@@ -21,18 +21,96 @@ lsp.setup()
 -- VScode-style pictograms
 local cmp = require('cmp')
 local lspkind = require('lspkind')
-local cmp_config = lsp.defaults.cmp_config({
+-- From AstroNvim
+local luasnip = require('luasnip')
+local border_opts =
+{ border = "single", winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None" }
+
+local function has_words_before()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
+local astro_settings = {
+    preselect = cmp.PreselectMode.None,
     formatting = {
+        fields = { "kind", "abbr", "menu" },
         format = lspkind.cmp_format({
             mode = 'symbol_text',
             maxwidth = 50,
             ellipsis_char = '...',
-            before = function (entry, vim_item)
+            before = function(entry, vim_item)
+                -- ?
                 return vim_item
             end
         })
-    }
-})
+    },
+    snippet = {
+        expand = function(args) luasnip.lsp_expand(args.body) end,
+    },
+    duplicates = {
+        nvim_lsp = 1,
+        luasnip = 1,
+        cmp_tabnine = 1,
+        buffer = 1,
+        path = 1,
+    },
+    confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+    },
+    window = {
+        completion = cmp.config.window.bordered(border_opts),
+        documentation = cmp.config.window.bordered(border_opts),
+    },
+    mapping = {
+        ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+        ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-y>"] = cmp.config.disable,
+        ["<C-e>"] = cmp.mapping {
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        },
+        ["<CR>"] = cmp.mapping.confirm { select = false },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+    },
+}
+
+local cmp_config = lsp.defaults.cmp_config(astro_settings)
 cmp.setup(cmp_config)
 
 -- Diagnostic messages
@@ -44,4 +122,3 @@ vim.diagnostic.config({
     update_in_insert = false,
     underline = true,
 })
-
