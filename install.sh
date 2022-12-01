20af27de63820363f1ce2c29dacdd8d99cb0c8cb
William Hleucka <william.hleucka@gmail.com>
2022-12-01 12:03:05 -0700

    Add cd -

--- install.sh
+++ install.sh
@@ -57,3 +57,4 @@ printf "Installing plugins...\n" && sleep 1
 $(which nvim) --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 printf "You have successfully installed neovim-config, have a great day!\n"
+cd -
