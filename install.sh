6501f426ca1dc1f0e0081ead8ccd0b262265a01f
William Hleucka <william.hleucka@gmail.com>
2022-12-01 11:36:53 -0700

    Patch install script

--- install.sh
+++ install.sh
@@ -1,40 +1,44 @@
 #!/usr/bin/env bash
 
 nvim_warning() {
-    printf "Oops! Install neovim and try again\n"
-    exit
+	printf "Oops! Install neovim and try again\n"
+	exit
 }
 
 git_warning() {
-    echo "Oops! Install git and try again\n"
-    exit
+	printf "Oops! Install git and try again\n"
+	exit
 }
 
 stow_warning() {
-    echo "Oops! Install stow and try again\n"
-    exit
+	printf "Oops! Install stow and try again\n"
+	exit
 }
 
 stow_error() {
-    echo "Error! Couldn't create stow link... :(\n"
-    exit
+	printf "Error! Couldn't create stow link... :(\n"
+	exit
 }
 
 rename_dir() {
-    mv $HOME/.config/nvim $HOME/.config/nvim.old
-    echo "Your neovig config has been transfered to ~/.config/nvim.old"
-    clone
+	mv $HOME/.config/nvim $HOME/.config/nvim.old
+	printf "Your neovig config has been transfered to ~/.config/nvim.old\n"
+	clone
 }
 
 clone() {
-    echo "Cloning repository..."
-    git clone https://github.com/whleucka/neovim-config ~/neovim-config
+	printf "Cloning repository...\n"
+	git clone https://github.com/whleucka/neovim-config ~/neovim-config
 }
 
 stow() {
-    echo "Stowing nvim..."
-    stow ~/neovim-config/nvim
-    nvim +PackerSync
+	printf "Stowing nvim...\n"
+	stow ~/neovim-config/nvim
+}
+
+install_packer() {
+	printf "Installing packer...\n"
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 }
 
 which nvim >/dev/null && echo "Neovim is installed" || nvim_warning
@@ -42,6 +46,10 @@ which git >/dev/null && echo "Git is installed" || git_warning
 which stow >/dev/null && echo "Stow is installed" || stow_warning
 
 [ -d "$HOME/.config/nvim" ] && rename_dir || clone
+[ ! -d "$HOME/.local/share/nvim/site/pack/packer" ] && install_packer
 [ -d "$HOME/neovim-config" ] && stow || stow_error
+
+printf "Installing plugins...\n"
+$(which nvim) --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 printf "You have successfully installed neovim-config, have a great day!\n"
