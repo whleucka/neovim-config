3b8256d3e30c75125299c50e03818a289199ba40
William Hleucka <william.hleucka@gmail.com>
2022-12-08 07:21:01 -0700

    patch: dressing

--- nvim/.config/nvim/lua/whleucka/config/dressing.lua
+++ nvim/.config/nvim/lua/whleucka/config/dressing.lua
@@ -1,10 +1,16 @@
 require('dressing').setup({
     input = {
         default_prompt = "➤ ",
-        winhighlight = "Normal:Normal,NormalNC:Normal",
+        win_options = {
+            winhighlight = "Normal:Normal,NormalNC:Normal",
+        },
     },
     select = {
         backend = { "telescope", "builtin" },
-        builtin = { winhighlight = "Normal:Normal,NormalNC:Normal" },
+        builtin = {
+            win_options = {
+                winhighlight = "Normal:Normal,NormalNC:Normal"
+            },
+        },
     },
 })
