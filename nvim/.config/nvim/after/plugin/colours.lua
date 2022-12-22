9840e2e7103294cf09bdf50ffe3da577f5621ebb
William Hleucka <william.hleucka@gmail.com>
2022-12-22 13:34:03 -0700

    feat: dynamic day/night

--- nvim/.config/nvim/after/plugin/colours.lua
+++ nvim/.config/nvim/after/plugin/colours.lua
@@ -1,4 +1,3 @@
 -- Load the colorscheme
 vim.cmd [[colorscheme tokyonight]]
 vim.cmd [[hi BufferTabpageFill guibg=NONE]]
-vim.cmd [[hi ColorColumn ctermbg=0 guibg=#2f384f]]
--- nvim/.config/nvim/lua/whleucka/config/tokyonight.lua
+++ nvim/.config/nvim/lua/whleucka/config/tokyonight.lua
@@ -1,7 +1,23 @@
+-- Returns either day or night theme, depending on the current time of day
+-- TODO dynamically set up / down
+local day_n_night = function()
+    local hour = tonumber(os.date("%H"))
+    local month = tonumber(os.date("%m"))
+    local dst_on = (month >= 3 and month <= 11) and true or false
+    local sun_up = dst_on and 6 or 8
+    local sun_down = dst_on and 18 or 16
+    if hour >= sun_up and hour <= sun_down then
+        vim.cmd [[hi ColorColumn ctermbg=0 guibg=#dddddd]]
+        return "day"
+    end
+    vim.cmd [[hi ColorColumn ctermbg=0 guibg=#2f384f]]
+    return "night"
+end
+
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  style = day_n_night(), -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = false, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
@@ -17,7 +33,7 @@ require("tokyonight").setup({
     floats = "normal", -- style for floating windows
   },
   sidebars = { "qf", "help", "terminal" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  day_brightness = 0.4, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
   dim_inactive = false, -- dims inactive windows
   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
