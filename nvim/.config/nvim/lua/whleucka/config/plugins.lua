3a440d445daeabc08c8db136c44403660cd42828
William Hleucka <william.hleucka@gmail.com>
2023-02-22 08:33:07 -0700

    Update with treehopper

--- nvim/.config/nvim/lua/whleucka/config/plugins.lua
+++ nvim/.config/nvim/lua/whleucka/config/plugins.lua
@@ -75,6 +75,18 @@ return {
         end
     },
 
+    -- Tree hopper
+    {
+        "mfussenegger/nvim-treehopper",
+        keys = { { "m", mode = { "o", "x" } } },
+        config = function()
+            vim.cmd([[
+        omap     <silent> m :<C-U>lua require('tsht').nodes()<CR>
+        xnoremap <silent> m :lua require('tsht').nodes()<CR>
+        ]])
+        end,
+    },
+
     -- Barbar (buffers)
     {
         'romgrk/barbar.nvim',
