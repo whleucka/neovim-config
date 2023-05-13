91237ff4bfded2a07716cb144b729b32abb1d79b
William Hleucka <william.hleucka@gmail.com>
2023-05-13 16:52:45 -0600

    fix: astro install dir

---
+++ astro.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ./astro/.config/nvim
+git clone https://github.com/whleucka/astronvim_config ./astro/.config/nvim/lua/user
